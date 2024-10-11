c534323b78febdb1307951021dd4655700842178
dayounglee <verdantjuly@kakao.com>
2024-10-11 16:41:37 +0900

    [Feat] LandingPage

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import Toolbar from "./Toolbar";
+
+export default function LandingPage(props) {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const onClickLogin = () => {
+    setIsLoggedIn(true);
+  };
+  const onClickLogout = () => {
+    setIsLoggedIn(false);
+  };
+  return (
+    <div>
+      <Toolbar
+        isLoggedIn={isLoggedIn}
+        onClickLogin={onClickLogin}
+        onClickLogout={onClickLogout}
+      />
+    </div>
+  );
+}
--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -1,11 +1,16 @@
 export default function Toolbar(props) {
-  const { isLoggedIn } = props;
+  const { isLoggedIn, onClickLogin, onClickLogout } = props;
   return (
     <div>
       {
         // 단축평가
         isLoggedIn && <span>환영합니다.</span>
       }
+      {isLoggedIn ? (
+        <button onClick={onClickLogout}>로그아웃</button>
+      ) : (
+        <button onClick={onClickLogin}>로그인</button>
+      )}
     </div>
   );
 }
